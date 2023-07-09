0da6cb2448ade11bc143f5fac0c9edbd2021b59e
DivaStress <divayudha4@gmail.com>
2023-07-09 21:17:32 +0700

    add one and two

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis in aspernatur ut mollitia? Quis iste at omnis illum rem quaerat suscipit corporis esse, voluptate molestias minus sapiente? Praesentium, laudantium dolorum.</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+    </ul>
 </body>
 </html>
